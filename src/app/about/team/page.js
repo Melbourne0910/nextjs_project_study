028e9c99cf42fd9532f27c4b3e509b19f5a62c99
yangneng@dp.tech <yangneng@dp.tech>
2026-06-30 21:26:00 +0800

    加两个文件结构即路由的页面文件

---
+++ src/app/about/team/page.js
@@ -0,0 +1,8 @@
+export default function TeamPage() {
+    return (
+        <div>
+            <h1>Our Team</h1>
+            <p>This is the team page of our Next.js project.</p>
+        </div>
+    )
+}
